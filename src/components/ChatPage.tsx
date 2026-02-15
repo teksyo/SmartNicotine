87bd432e432786ec0ed05e72234e9f2eec381705
teksyo <muhammadfaizan@teksyo.com>
2026-02-15 22:12:16 +0500

    added variables

--- src/components/ChatPage.tsx
+++ src/components/ChatPage.tsx
@@ -83,13 +83,15 @@ const ChatPage = () => {
       "last_name": answers['Last name'] || '',
       "date_of_birth": answers['Date of birth'] || '',
       "email": profile.email || '',
+      "sex": answers['Are you Male or Female?'] || '',
       "family_role": answers['Which of these best describes you?'] || '',
       "smoking_duration": answers['How long have you smoked cigarettes?'] || '',
       "brand": answers['What is your preferred cigarette brand?'] || '',
       "cigs_per_day": answers['On average, how many cigarettes do you smoke per day?'] || '',
       "quit_attempts": answers['How many times have you seriously tried to quit smoking?'] || '',
       "long_quits": answers['How many times have you quit smoking for 30 days or more?'] || '',
-      "motivation": answers['What is your main motivation for wanting to quit smoking?'] || ''
+      "motivation": answers['What is your main motivation for wanting to quit smoking?'] || '',
+      "how_many_30_days": answers['(LAST QUESTION) How many times have you quit smoking for 30 days or more?'] || '',
     };
   };
 
